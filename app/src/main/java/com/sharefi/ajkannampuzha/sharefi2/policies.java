222166f19c765b299c25ba4a4430a08f4bfc8e69
Antony Johnson <ajkannampuzha@mail.com>
2016-09-04 14:44:19 +0530

    Ready to push to master. Next: Check if policies work well

--- app/src/main/java/com/sharefi/ajkannampuzha/sharefi2/policies.java
+++ app/src/main/java/com/sharefi/ajkannampuzha/sharefi2/policies.java
@@ -1,8 +1,13 @@
 package com.sharefi.ajkannampuzha.sharefi2;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -13,6 +18,11 @@ public class policies extends AppCompatActivity {
     public final String memail=bundle.getString("email");
     public final String mpassword=bundle.getString("password");
     public int count=0;
+    public static final int WIFI_STATE=0;
+    public static final int CHANGE_WIFI=0;
+    public static final int COARSE_LOCATION=0;
+    public static final int FINE_LOCATION=0;
+    public static final int NETWORK_STATE=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +40,24 @@ public class policies extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
             {
                 if ( isChecked )
-                {
+                {   Log.d("FIrst","In here");
                     count++;
+                    if (ContextCompat.checkSelfPermission(policies.this,
+                            Manifest.permission.ACCESS_WIFI_STATE)
+                            != PackageManager.PERMISSION_GRANTED) {
+                            // No explanation needed, we can request the permission.
+                            ActivityCompat.requestPermissions(policies.this,
+                                    new String[]{Manifest.permission.ACCESS_WIFI_STATE},
+                                    WIFI_STATE);
+                    }
+                    if (ContextCompat.checkSelfPermission(policies.this,
+                            Manifest.permission.CHANGE_WIFI_STATE)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        // No explanation needed, we can request the permission.
+                        ActivityCompat.requestPermissions(policies.this,
+                                new String[]{Manifest.permission.CHANGE_WIFI_STATE},
+                                CHANGE_WIFI);
+                    }
 
                 }else{
                     count--;
@@ -46,6 +72,23 @@ public class policies extends AppCompatActivity {
                 if ( isChecked )
                 {
                     count++;
+                    if (ContextCompat.checkSelfPermission(policies.this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        // No explanation needed, we can request the permission.
+                        ActivityCompat.requestPermissions(policies.this,
+                                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                COARSE_LOCATION);
+                    }
+                    if (ContextCompat.checkSelfPermission(policies.this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        // No explanation needed, we can request the permission.
+                        ActivityCompat.requestPermissions(policies.this,
+                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                FINE_LOCATION);
+                    }
+
 
                 }else{
                     count--;
@@ -60,6 +103,14 @@ public class policies extends AppCompatActivity {
                 if ( isChecked )
                 {
                     count++;
+                    if (ContextCompat.checkSelfPermission(policies.this,
+                            Manifest.permission.ACCESS_NETWORK_STATE)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        // No explanation needed, we can request the permission.
+                        ActivityCompat.requestPermissions(policies.this,
+                                new String[]{Manifest.permission.ACCESS_NETWORK_STATE},
+                                NETWORK_STATE);
+                    }
 
                 }else{
                     count--;
@@ -97,4 +148,5 @@ public class policies extends AppCompatActivity {
 
         }
     }
+
 }
